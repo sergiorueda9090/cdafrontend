fea07d4177ebb24b3e010344c2ba0a06dcaba23c
sergiorueda9090 <sergiorueda90@hotmail.com>
2025-08-29 08:01:49 -0500

    websocket build

--- src/components/UsersConnected.jsx
+++ src/components/UsersConnected.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState, useRef } from "react";
 import AvatarGroup from '@mui/material/AvatarGroup';
 import { Tooltip, Avatar } from "@mui/material";
 import { styled } from '@mui/material/styles';
